10c9f7c67f2f46e42b253712e555624fb6f62d86
Cameron Tacklind <cameron@tacklind.com>
2020-12-27 22:16:49 -0800

    Docs

--- src/utils/io.ts
+++ src/utils/io.ts
@@ -2,6 +2,14 @@ import { getInput, debug as ghDebug } from '@actions/core';
 import { isGitHubAction } from './isGitHubAction';
 import { formatWithOptions } from 'util';
 
+/**
+ * Read Github Action `with` field from workflow yml
+ * @see https://docs.github.com/en/free-pro-team@latest/actions/reference/workflow-syntax-for-github-actions#jobsjob_idstepswith
+ *
+ * Often used for public and secret configuration
+ *
+ * @param name name of the input field
+ */
 export async function input(name: string): Promise<string> {
   if (!isGitHubAction) {
     throw new Error('Not made to work in other environments yet!');
